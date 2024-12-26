c05278c97e2c71efe1e418a792fa2a7ae4f5cfb0
o06660o <2957531723@qq.com>
2024-12-26 21:31:10 +0800

    fix: fix bugs when scaling

--- src/level_dream.py
+++ src/level_dream.py
@@ -18,6 +18,7 @@ class LevelDream(Level):
             self.obstacle_sprites,
             self.create_attack,
         )
+        self.scale *= ENV.LEVEL_DREAM.IMAGE_SCALE
         # floor = pygame.image.load("assets/graphics/environment/dream/floor.png")
         plat_main = pygame.image.load("assets/graphics/environment/dream/dream_BG_plats_0000_4.png")
         plat_large = pygame.image.load(
@@ -29,8 +30,9 @@ class LevelDream(Level):
         plat_small = pygame.image.load(
             "assets/graphics/environment/dream/platform/dream_plat_small.png"
         )
+        print(self.scale)
         Tile(
-            self.scale * ENV.LEVEL_DREAM.IMAGE_SCALE,
+            self.scale,
             (-10 * self.scale, 0),
             [self.visible_sprites, self.obstacle_sprites],
             "blocks",
@@ -38,71 +40,71 @@ class LevelDream(Level):
             ENV.LEVEL_DREAM.PLAT_HITBOX_OFFSET,
         )
         Tile(
-            self.scale * ENV.LEVEL_DREAM.IMAGE_SCALE,
-            (160 * self.scale, 30 * self.scale),
+            self.scale,
+            (80 * self.scale, 15 * self.scale),
             [self.visible_sprites, self.obstacle_sprites],
             "blocks",
             plat_large,
             ENV.LEVEL_DREAM.PLAT_LARGE_HITBOX_OFFSET,
         )
         Tile(
-            self.scale * ENV.LEVEL_DREAM.IMAGE_SCALE,
-            (300 * self.scale, 30 * self.scale),
+            self.scale,
+            (150 * self.scale, 15 * self.scale),
             [self.visible_sprites, self.obstacle_sprites],
             "blocks",
             plat_large,
             ENV.LEVEL_DREAM.PLAT_LARGE_HITBOX_OFFSET,
         )
         Tile(
-            self.scale * ENV.LEVEL_DREAM.IMAGE_SCALE,
-            (440 * self.scale, 30 * self.scale),
+            self.scale,
+            (220 * self.scale, 15 * self.scale),
             [self.visible_sprites, self.obstacle_sprites],
             "blocks",
             plat_large,
             ENV.LEVEL_DREAM.PLAT_LARGE_HITBOX_OFFSET,
         )
         Tile(
-            self.scale * ENV.LEVEL_DREAM.IMAGE_SCALE,
-            (-120 * self.scale, 30 * self.scale),
+            self.scale,
+            (-60 * self.scale, 15 * self.scale),
             [self.visible_sprites, self.obstacle_sprites],
             "blocks",
             plat_large,
             ENV.LEVEL_DREAM.PLAT_LARGE_HITBOX_OFFSET,
         )
         Tile(
-            self.scale * ENV.LEVEL_DREAM.IMAGE_SCALE,
-            (-160 * self.scale, -80 * self.scale),
+            self.scale,
+            (-80 * self.scale, -40 * self.scale),
             [self.visible_sprites, self.obstacle_sprites],
             "blocks",
             plat_mid,
             ENV.LEVEL_DREAM.PLAT_MID_HITBOX_OFFSET,
         )
         Tile(
-            self.scale * ENV.LEVEL_DREAM.IMAGE_SCALE,
-            (0 * self.scale, -150 * self.scale),
+            self.scale,
+            (0 * self.scale, -75 * self.scale),
             [self.visible_sprites, self.obstacle_sprites],
             "blocks",
             plat_small,
             ENV.LEVEL_DREAM.PLAT_SMALL_HITBOX_OFFSET,
         )
         Tile(
-            self.scale * ENV.LEVEL_DREAM.IMAGE_SCALE,
-            (170 * self.scale, -180 * self.scale),
+            self.scale,
+            (85 * self.scale, -90 * self.scale),
             [self.visible_sprites, self.obstacle_sprites],
             "blocks",
             plat_main,
             ENV.LEVEL_DREAM.PLAT_HITBOX_OFFSET,
         )
         Tile(
-            self.scale * ENV.LEVEL_DREAM.IMAGE_SCALE,
-            (560 * self.scale, 0 * self.scale),
+            self.scale,
+            (280 * self.scale, 0 * self.scale),
             [self.visible_sprites, self.obstacle_sprites],
             "blocks",
             plat_main,
             ENV.LEVEL_DREAM.PLAT_HITBOX_OFFSET,
         )
         # Tile(
-        #     self.scale * ENV.LEVEL_DREAM.IMAGE_SCALE,
+        #     self.scale *,
         #     (-120 * self.scale, 150 * self.scale),
         #     [self.visible_sprites, self.obstacle_sprites],
         #     "blocks",
